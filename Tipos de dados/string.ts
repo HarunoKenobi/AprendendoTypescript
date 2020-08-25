c66f88347f84efa7f6870a57e5999a5eb6bb5d12
Arthur Guilherme Carvalho de Souza <arthur.souza@itixti.com.br>
2020-08-25 01:02:42 -0300

    Adicionando tipo string

---
+++ Tipos de dados/string.ts
@@ -0,0 +1,7 @@
+let primeiroNome: string = "Haruno";
+let ultimoNome: string = 'Kenobi';
+let nomeCompleto: string = `O nome completo é ${primeiroNome} ${ultimoNome}`;
+
+console.log(primeiroNome);
+console.log(ultimoNome);
+console.log(nomeCompleto);
