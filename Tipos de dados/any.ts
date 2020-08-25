cc3d0b2a3cea932e603f18508c1f246392e1560c
Arthur Guilherme Carvalho de Souza <arthur.souza@itixti.com.br>
2020-08-25 01:44:00 -0300

    Adicionando tipo any

---
+++ Tipos de dados/any.ts
@@ -0,0 +1,10 @@
+let valorNaoIdentificado: any;
+
+valorNaoIdentificado = 4;
+console.log(valorNaoIdentificado);
+
+valorNaoIdentificado = "agora é uma string";
+console.log(valorNaoIdentificado);
+
+valorNaoIdentificado = false;
+console.log(valorNaoIdentificado);  
