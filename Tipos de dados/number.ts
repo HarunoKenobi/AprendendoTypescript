ac9142b00c7d5a6ec72bf8ee6037df94df54c6b4
Arthur Guilherme Carvalho de Souza <arthur.souza@itixti.com.br>
2020-08-25 00:56:40 -0300

    Adicionando tipo number

---
+++ Tipos de dados/number.ts
@@ -0,0 +1,11 @@
+let valorInteiro: number = 1;
+let valorDecimal: number = 1.2;
+let valorHexadecimal: number = 0x7d5;
+let valorBinario: number = 0b11111011101;
+let valorOctal: number = 0o3744;
+
+console.log(valorInteiro);
+console.log(valorDecimal);
+console.log(valorHexadecimal);
+console.log(valorBinario);
+console.log(valorOctal);
