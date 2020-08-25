b6ea92e13f311389c5906bbf1e79dad12f4979be
Arthur Guilherme Carvalho de Souza <arthur.souza@itixti.com.br>
2020-08-25 01:44:11 -0300

    Adicioando tipo void

---
+++ Tipos de dados/void.ts
@@ -0,0 +1,10 @@
+let valorVoid1: void = null;
+let valorVoid2: void = undefined;
+
+function aviso(): void{
+    console.log("aviso");
+}
+
+console.log(valorVoid1);
+console.log(valorVoid2);
+aviso();
